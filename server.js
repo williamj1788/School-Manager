e2484a71d98b1557d6a2e8ec9851a15448535a96
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-13 17:26:37 -0400

    connect mongoose to backend

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const upload = multer();
 const mongoose = require('mongoose');
 
 const app = express();
+const url = 'mongodb+srv://quez:quez123@project-cluster-8qd4n.mongodb.net/SchoolManager?retryWrites=true';
+mongoose.connect(url, {useNewUrlParser: true})
+    .then(() => console.log('connected to database'))
+    .catch(err => console.log(err));
 
 app.use(upload.none());
 app.use(session({secret: 'key board cat'}));
+
+
 
 const PORT = 8080;
 
