d1bd5e21ac0f862f5449b5e82d5503bd1ca77652
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-16 14:14:49 -0400

    started to incorpate redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch,BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './styles/Normalize.css';
 
 import Login from './components/login';
 import Signup from './components/signup';
 import Dashboard from './components/dashboard';
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Switch>
-                        <Route exact path="/" component={Login} />
-                        <Route path="/signup" component={Signup} />
-                        <Route path="/dashboard" component={Dashboard} />
-                    </Switch>
-                </div>
-            </Router>
-        );
-    }
-}
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/dashboard" component={Dashboard} />
+                </Switch>
+            </div>
+        </Router>
+    </Provider>
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
---
+++ client/src/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    user: null,
+};
+
+function reducer(state = initialState,action){
+    return state;
+}
+
+export default reducer;
