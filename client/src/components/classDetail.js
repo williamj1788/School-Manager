e3996811e4df17dc84b4c29fdac0679643a865f6
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-17 19:05:23 -0400

    can now select different

--- client/src/components/classDetail.js
+++ client/src/components/classDetail.js
@@ -7,7 +7,32 @@ import { connect } from "react-redux";
 const mapStateToProps = state => {
     return { tasks: state.tasks, tests: state.tests };
 }; 
+
 class ClassDetail extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            showTask: true,
+            showTest: false,
+        }
+        this.SetShowtask = this.SetShowtask.bind(this);
+        this.SetShowTest = this.SetShowTest.bind(this);
+    }
+
+    SetShowTest(){
+        this.setState({
+            showTask: false,
+            showTest: true,
+        });
+    }
+
+    SetShowtask(){
+        this.setState({
+            showTask: true,
+            showTest: false,
+        });
+    }
+
 
     render(){
         let tasks = this.props.tasks;
@@ -29,11 +54,11 @@ class ClassDetail extends React.Component{
                         <span id="class-header-text">Class Name</span>
                     </div>
                     <div id="tab-container">
-                        <div className="tab">
-                            <span className="centered">Task</span>
+                        <div className="tab" onMouseDown={this.SetShowtask} style={this.state.showTask ? {backgroundColor: '#06CAF2'} : null} >
+                            <span className="centered" style={this.state.showTask ? {color: 'white'} : null}>Task</span>
                         </div>
-                        <div className="tab">
-                            <span className="centered">Test</span>
+                        <div className="tab" onMouseDown={this.SetShowTest} style={this.state.showTest ? {backgroundColor: '#06CAF2'} : null}>
+                            <span className="centered" style={this.state.showTest ? {color: 'white'} : null}>Test</span>
                         </div>
                     </div>
                     <div className="detail-container">
