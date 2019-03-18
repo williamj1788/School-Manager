a3fdf4ed3e9cee08095897ae36ce5b212df4e8fd
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-18 15:56:03 -0400

    added task modal

--- client/src/components/addTask.js
+++ client/src/components/addTask.js
@@ -4,7 +4,22 @@ class AddTask extends React.Component{
 
     render(){
         return(
-            <h1>Hello Task</h1>
+            <div className="addTask">
+                <div id="addTask-header">
+                    <span id="addTask-header-text">Add Task</span>
+                </div>
+                <div className="offset">
+                    <form id="addTask-form">
+                        <label htmlFor="taskName">
+                        Name: <input type="text" name="taskName" placeholder="Enter Name"/>
+                        </label>
+                        <label htmlFor="dueDate">
+                       Due Date: <input type="date" name="dueDate" placeholder ="Enter Due Date"/>
+                        </label>
+                        <button id="submit-task" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
