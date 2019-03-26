5ed1574a2f13864fc2f7647cb8408ae2c5b521b5
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-25 21:07:11 -0400

    added path to express static to fix bugs

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/user', userRouter);
 app.use('/api/class', classRouter);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
         res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
