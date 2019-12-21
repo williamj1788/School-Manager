868f67043ab192f40700d8a78c3b3e26d4324cc2
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-21 12:30:29 -0500

    feature: added material ui

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,57 +1,75 @@
-import React from 'react';
-import '../styles/Main.scss';
-import {Redirect, Link} from 'react-router-dom';
+import React from "react";
+import "../styles/Main.scss";
+import { Redirect, Link } from "react-router-dom";
 
-export class Signup extends React.Component{
-    state = {
-        signIN: false,
-        error: false,
-    }
-    
-    handleSubmit = event =>{
-        event.preventDefault();
-        
-        let form = document.getElementsByClassName('main-form')[0];
-        let formData = new FormData(form);
-        
-        fetch('/api/user/signup',{
-            method: 'POST',
-            body: formData,
-            credentials: 'include',
-        })
-        .then(res => {
-            if(res.status !== 400){
-                this.setState({
-                    signIN: true,
-                });
-            }else{
-                this.setState({
-                    error: true,
-                });
-            }
-            form.reset();
-        })
-    }
-    
-    render(){
-        if(this.state.signIN) return <Redirect to="/dashboard" />;
-        
-        return(
-            <div className="Main">
-                <form className="main-form" onSubmit={this.handleSubmit}>
-                    <p className="form-title">School Manager</p>
-                    <input type="text" className="input-field" name="username" placeholder="Username" autoComplete="off" minLength='3' required/>
-                    <input type="password" className="input-field" name="password" placeholder="Password" minLength='3' required/>
-                    <button className="submit-button" type="submit">Sign Up</button>
-                    {this.state.error && <p className="error">Username already taken</p>}
-                    <div className="account">
-                        <p>Already have an account?</p>
-                        <Link to="/" id="account-link">Login</Link>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+export class Signup extends React.Component {
+  state = {
+    signIN: false,
+    error: false
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    let form = document.getElementsByClassName("main-form")[0];
+    let formData = new FormData(form);
+
+    fetch("/api/user/signup", {
+      method: "POST",
+      body: formData,
+      credentials: "include"
+    }).then(res => {
+      if (res.status !== 400) {
+        this.setState({
+          signIN: true
+        });
+      } else {
+        this.setState({
+          error: true
+        });
+      }
+      form.reset();
+    });
+  };
+
+  render() {
+    if (this.state.signIN) return <Redirect to="/dashboard" />;
+
+    return (
+      <div className="Main">
+        <form className="main-form" onSubmit={this.handleSubmit}>
+          <p className="form-title">StudyBit</p>
+          <input
+            type="text"
+            className="input-field"
+            name="username"
+            placeholder="Username"
+            autoComplete="off"
+            minLength="3"
+            required
+          />
+          <input
+            type="password"
+            className="input-field"
+            name="password"
+            placeholder="Password"
+            minLength="3"
+            required
+          />
+          <button className="submit-button" type="submit">
+            Sign Up
+          </button>
+          {this.state.error && <p className="error">Username already taken</p>}
+          <div className="account">
+            <p>Already have an account?</p>
+            <Link to="/" id="account-link">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Signup;
--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Switch,BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-import './styles/Normalize.css';
+import "./styles/Normalize.css";
+import "./styles/index.scss";
 
-import Login from './components/login';
-import Signup from './components/signup';
-import Dashboard from './components/dashboard/dashboard';
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Dashboard from "./components/dashboard/dashboard";
 
 const Root = ({ store }) => (
-    <Provider store={store}>
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route path="/signup" component={Signup} />
-                    <Route path="/dashboard" component={Dashboard} />
-                </Switch>
-            </div>
-        </Router>
-    </Provider>
-)
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
+);
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'));
-
+ReactDOM.render(<Root store={store} />, document.getElementById("root"));
