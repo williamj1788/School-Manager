ad7989f7cf05fca578eee7ddfd1fe8173150f9b5
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-25 17:19:34 -0500

    fix: added config file for ci

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const env = process.env.NODE_ENV;
+module.exports = {
+  dbUrl:
+    env === 'production' ? process.env.PRODUCTION_DB_URL :
+    env === 'testing' ? process.env.TEST_DB_URL :
+    env === 'ci' ? process.env.CI_DB_URL :
+    process.env.DEV_DB_URL,
+  sessionSecret: process.env.SESSION_SECRET,
+  env,
+};
