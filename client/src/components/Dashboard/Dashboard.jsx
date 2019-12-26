dd11fb60d4db1bbc79c730286e05ee86c2fdf30d
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-26 16:08:49 -0500

    feature: added tabs and drawer

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -9,6 +9,14 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Paper from "@material-ui/core/Paper";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import { makeStyles } from "@material-ui/core/styles";
 
 import MenuIcon from "@material-ui/icons/Menu";
 import SettingsIcon from "@material-ui/icons/Settings";
@@ -16,13 +24,26 @@ import ScheduleIcon from "@material-ui/icons/Schedule";
 import CheckSharpIcon from "@material-ui/icons/CheckSharp";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 
+const useStyle = makeStyles({
+  container: {
+    height: "200vh"
+  },
+  paper: {
+    position: "sticky"
+  },
+  list: {
+    width: 250
+  }
+});
+
 function Dashboard() {
-  const [tab, setTab] = useState(0);
+  const classes = useStyle();
+  const [open, setOpen] = useState(false);
   return (
-    <Container>
-      <AppBar>
+    <React.Fragment>
+      <AppBar position="sticky">
         <ToolBar>
-          <IconButton color="inherit">
+          <IconButton color="inherit" onClick={() => setOpen(o => !o)}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className="dashboard-title">
@@ -32,25 +53,66 @@ function Dashboard() {
             <SettingsIcon />
           </IconButton>
         </ToolBar>
-        <Tabs variant="fullWidth" value={tab} centered>
-          <Tab
-            label="Schedule"
-            icon={<ScheduleIcon />}
-            onClick={() => setTab(0)}
-          />
-          <Tab
-            label="Task"
-            icon={<CheckSharpIcon />}
-            onClick={() => setTab(1)}
-          />
-          <Tab
-            label="Exams"
-            icon={<AssignmentIcon />}
-            onClick={() => setTab(2)}
-          />
-        </Tabs>
+        <TabsNav />
       </AppBar>
-    </Container>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List className={classes.list}>
+          <ListItem button>
+            <ListItemIcon>
+              <MenuIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <MenuIcon />
+            </ListItemIcon>
+            <ListItemText primary="Classes" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <MenuIcon />
+            </ListItemIcon>
+            <ListItemText primary="Tasks" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <MenuIcon />
+            </ListItemIcon>
+            <ListItemText primary="Exams" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <MenuIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <Container className={classes.container}>sdfsd</Container>
+    </React.Fragment>
+  );
+}
+
+function TabsNav() {
+  const classes = useStyle();
+  const [tab, setTab] = useState(0);
+  return (
+    <Paper className={classes.paper} elevation={0} square>
+      <Tabs variant="fullWidth" value={tab} centered>
+        <Tab
+          label="Schedule"
+          icon={<ScheduleIcon />}
+          onClick={() => setTab(0)}
+        />
+        <Tab label="Task" icon={<CheckSharpIcon />} onClick={() => setTab(1)} />
+        <Tab
+          label="Exams"
+          icon={<AssignmentIcon />}
+          onClick={() => setTab(2)}
+        />
+      </Tabs>
+    </Paper>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import { ThemeProvider } from "@material-ui/core/styles";
+
 import "./styles/Normalize.css";
 import "./styles/index.scss";
 
@@ -13,15 +15,17 @@ import Dashboard from "./components/Dashboard/Dashboard";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={AuthPage} />
-          <Route path="/signup" component={AuthPage} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={AuthPage} />
+            <Route path="/signup" component={AuthPage} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>  
+    </ThemeProvider>
   </Provider>
 );
 
